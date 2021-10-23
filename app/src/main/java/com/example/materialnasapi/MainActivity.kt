eebdb6517064daef445e6fcefca23178c9b55a31
tkaya <tkayasat@gmail.com>
2021-10-23 06:51:03 +0300

    часть дз2

--- app/src/main/java/com/example/materialnasapi/MainActivity.kt
+++ app/src/main/java/com/example/materialnasapi/MainActivity.kt
@@ -9,6 +9,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setTheme(R.style.NavyTheme);
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.container, MainFragment.newInstance())
